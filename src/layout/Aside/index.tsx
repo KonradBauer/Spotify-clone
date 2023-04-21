6cc5b8bf400df98a98c4770e1e8ed0319919dfc0
Konrad Bauer <konradbauer94@gmail.com>
2023-04-21 13:04:40 +0200

    added homepage to package.json, added Home, Search and Library component into Aside

--- src/layout/Aside/index.tsx
+++ src/layout/Aside/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import {StyledAside, StyledLogo} from "./styled";
+import {Home, Library, Search, StyledAside, StyledLogo} from "./styled";
+
 
 export const Aside: React.FC = () => {
     return (
         <StyledAside>
             <StyledLogo/>
+            <Home/>
+            <Search/>
+            <Library/>
         </StyledAside>
     )
 }
--- src/layout/Aside/styled.tsx
+++ src/layout/Aside/styled.tsx
@@ -5,10 +5,27 @@ export const StyledAside = styled.div`
   display: grid;
   background-color: ${({theme}) => theme.color.black};
   grid-area: 1 / 1 / 6 / 2;
+  align-items: flex-start;
 `
 
 export const StyledLogo = styled(Logo)`
   margin: 14px -10px;
   width: 75%;
   height: 60px;
+  cursor: pointer;
+`
+
+export const Home = styled.div`
+  width: 255px;
+  height: 40px;
+`
+
+export const Search = styled.div`
+  width: 255px;
+  height: 40px;
+`
+
+export const Library = styled.div`
+  width: 255px;
+  height: 40px;
 `
