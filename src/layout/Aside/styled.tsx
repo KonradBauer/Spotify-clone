21094f063603e9cf69b14be393f3fb24459d537f
Konrad Bauer <konradbauer94@gmail.com>
2023-04-25 22:50:07 +0200

    add hover to navigation with color, change cursor and add some transition (0.2s)

--- src/layout/Aside/styled.tsx
+++ src/layout/Aside/styled.tsx
@@ -1,55 +1,61 @@
 import styled from "styled-components";
-import {ReactComponent as Logo} from "../../common/images/logo.svg";
-import {ReactComponent as House} from "../../common/images/home.svg";
-import {ReactComponent as HomeActive} from "../../common/images/home-active.svg";
-import {ReactComponent as Loupe} from "../../common/images/loupe.svg";
-import {ReactComponent as LoupeActive} from "../../common/images/loupe-active.svg";
-import {ReactComponent as Lib} from "../../common/images/lib.svg";
-import {ReactComponent as LibActive} from "../../common/images/lib-active.svg";
+import { ReactComponent as Logo } from "../../common/images/logo.svg";
+import { ReactComponent as House } from "../../common/images/home.svg";
+import { ReactComponent as HomeActive } from "../../common/images/home-active.svg";
+import { ReactComponent as Loupe } from "../../common/images/loupe.svg";
+import { ReactComponent as LoupeActive } from "../../common/images/loupe-active.svg";
+import { ReactComponent as Lib } from "../../common/images/lib.svg";
+import { ReactComponent as LibActive } from "../../common/images/lib-active.svg";
 
 export const StyledAside = styled.div`
   display: grid;
-  background-color: ${({theme}) => theme.color.black};
+  background-color: ${({ theme }) => theme.color.black};
   grid-area: 1 / 1 / 6 / 2;
   align-items: flex-start;
-`
+`;
 export const List = styled.ul`
-  color: ${({theme}) => theme.color.nobel};
+  color: ${({ theme }) => theme.color.nobel};
   font-weight: bold;
   font-size: 14px;
   list-style-type: none;
-  margin-bottom: 450px;;
+  margin-bottom: 450px;
   display: flex;
   flex-direction: column;
   padding: 0 0 0 30px;
-
-`
-
-
+`;
 export const StyledLogo = styled(Logo)`
-  color: ${({theme}) => theme.color.white};
+  color: ${({ theme }) => theme.color.white};
   margin: 14px -10px;
   width: 75%;
   height: 60px;
   cursor: pointer;
-`
-export const StyledHouse = styled(House)`margin-right: 15px`
-export const StyledLib = styled(Lib)`margin-right: 15px`
-export const StyledLoupe = styled(Loupe)`margin-right: 15px`
+`;
+export const StyledHouse = styled(House)`
+  margin-right: 15px;
+`;
+export const StyledLib = styled(Lib)`
+  margin-right: 15px;
+`;
+export const StyledLoupe = styled(Loupe)`
+  margin-right: 15px;
+`;
 export const Home = styled.span`
   display: flex;
   justify-content: flex-start;
   width: 255px;
   height: 40px;
   cursor: pointer;
+  transition: 0.2s;
 
-
-  &:active {
-    color: ${({theme}) => theme.color.white};
-    transform: scale(0.99);
+  &:hover {
+    color: ${({ theme }) => theme.color.white};
   }
 
-`
+  &:active {
+    color: ${({ theme }) => theme.color.white};
+    transform: scale(0.99);
+  }
+`;
 
 export const Search = styled.span`
   display: flex;
@@ -57,13 +63,17 @@ export const Search = styled.span`
   width: 255px;
   height: 40px;
   cursor: pointer;
+  transition: 0.2s;
 
-  &:active {
-    color: ${({theme}) => theme.color.white};
-    transform: scale(0.99);
+  &:hover {
+    color: ${({ theme }) => theme.color.white};
   }
 
-`
+  &:active {
+    color: ${({ theme }) => theme.color.white};
+    transform: scale(0.99);
+  }
+`;
 
 export const Library = styled.span`
   display: flex;
@@ -71,10 +81,14 @@ export const Library = styled.span`
   width: 255px;
   height: 40px;
   cursor: pointer;
+  transition: 0.2s;
 
-  &:active {
-    color: ${({theme}) => theme.color.white};
-    transform: scale(0.99);
+  &:hover {
+    color: ${({ theme }) => theme.color.white};
   }
 
-`
+  &:active {
+    color: ${({ theme }) => theme.color.white};
+    transform: scale(0.99);
+  }
+`;
