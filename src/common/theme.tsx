6212f20c76aa3d9eb4086e5a4c40a9dc21c4ffb0
Konrad Bauer <konradbauer94@gmail.com>
2023-04-25 22:47:26 +0200

    added new colort to theme: mineShaft

--- src/common/theme.tsx
+++ src/common/theme.tsx
@@ -1,21 +1,22 @@
 export const theme = {
-    color: {
-        codGray: "#121212",
-        black: "#000000",
-        white: "#FFFFFF",
-        nobel: "#B3B3B3",
-        malachite: "#1ed760"
-    },
+  color: {
+    codGray: "#121212",
+    black: "#000000",
+    white: "#FFFFFF",
+    nobel: "#B3B3B3",
+    malachite: "#1ed760",
+    mineShaft: "#242424",
+  },
 
-    breakpoints: {
-        theSmallestMobileView: 290,
-        mobileS: 320,
-        mobileM: 425,
-        mobileMax: 847,
-        tabletVerticalMax: 991,
-        beforeMobileMax: 1000,
-        tabletHorizontalMax: 1199,
-        desktopHorizontal: 1250,
-        desktopHorizontalMax: 1672,
-    },
+  breakpoints: {
+    theSmallestMobileView: 290,
+    mobileS: 320,
+    mobileM: 425,
+    mobileMax: 847,
+    tabletVerticalMax: 991,
+    beforeMobileMax: 1000,
+    tabletHorizontalMax: 1199,
+    desktopHorizontal: 1250,
+    desktopHorizontalMax: 1672,
+  },
 };
