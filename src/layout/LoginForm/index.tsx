9fd473fcd2317a1b96134c03354fff8e6ac48eb2
Konrad Bauer <konradbauer94@gmail.com>
2023-04-27 20:52:25 +0200

    import loginUrl to LoginForm component

--- src/layout/LoginForm/index.tsx
+++ src/layout/LoginForm/index.tsx
@@ -1,28 +1,35 @@
 import React from "react";
-import {Container, Form, Input, Label, Line, LogInButton, StyledLogo} from "./styled";
+import {
+  Container,
+  Form,
+  Input,
+  Label,
+  Line,
+  LogInButton,
+  StyledLogo,
+} from "./styled";
+import { loginUrl } from "../../core/getAPI";
 
 interface Form {
-    Input: string
-    placeholder: string
+  Input: string;
+  placeholder: string;
 }
 
 export const LoginForm: React.FC = () => {
+  const btnText: string = "zaloguj się";
+  const uppercaseText: string = btnText.toUpperCase();
 
-    const btnText: string = "zaloguj się"
-    const uppercaseText: string = btnText.toUpperCase();
-
-    return (
-        <>
-            <Container>
-                <StyledLogo/>
-                <Line/>
-                <Form>
-                    <Input placeholder={"Login"}/>
-                    <Input type="password" placeholder={"Hasło"}/>
-                </Form>
-                <LogInButton>{uppercaseText}</LogInButton>
-            </Container>
-
-        </>
-    )
-}
+  return (
+    <>
+      <Container>
+        <StyledLogo />
+        <Line />
+        <Form>
+          <Input placeholder={"Login"} />
+          <Input type="password" placeholder={"Hasło"} />
+        </Form>
+        <LogInButton href={loginUrl}>{uppercaseText}</LogInButton>
+      </Container>
+    </>
+  );
+};
