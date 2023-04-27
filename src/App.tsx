930387b0154bd503bc7e4551f324f29353b462ce
Konrad Bauer <konradbauer94@gmail.com>
2023-04-27 20:53:25 +0200

    create a getAPI file with login properties data

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,9 @@ import { GlobalContainer } from "./layout/GlobalContainer";
 import { Aside } from "./layout/Aside";
 import { Main } from "./layout/Main";
 // import { Container } from "./layout/LoginForm/styled";
-// import { LoginForm } from "./layout/LoginForm";
+import { LoginForm } from "./layout/LoginForm";
 
 const App: React.FC = () => {
-  return (
-    <GlobalContainer>
-      <Aside />
-      <Main />
-    </GlobalContainer>
-  );
+  return <LoginForm />;
 };
 export default App;
