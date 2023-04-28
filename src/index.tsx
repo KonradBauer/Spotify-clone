53774aaa58875858480d8d832fc1a7bbb72ef35e
Konrad Bauer <konradbauer94@gmail.com>
2023-04-28 22:52:19 +0200

    add DataLayer provider in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {ThemeProvider} from "styled-components";
-import {theme} from "./common/theme";
-import {GlobalStyle} from "./common/GlobalStyle";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./common/theme";
+import { GlobalStyle } from "./common/GlobalStyle";
+import { DataLayer } from "./core/DataLayer";
+import { initialState, reducer } from "./core/reducer";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <GlobalStyle/>
-        <ThemeProvider theme={theme}>
-            <App/>
-        </ThemeProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <DataLayer initialState={initialState} reducer={reducer}>
+        <App />
+      </DataLayer>
+    </ThemeProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
