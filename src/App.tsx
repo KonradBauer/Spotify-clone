8a98507d34667fef416484be6b680aacbda5aea6
Konrad Bauer <konradbauer94@gmail.com>
2023-04-30 18:56:23 +0200

    delete useState import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { GlobalContainer } from "./layout/GlobalContainer";
 import { Aside } from "./layout/Aside";
 import { Main } from "./layout/Main";
