b357595dca9782a592e9e30a65ba16ebc68ae1e1
Konrad Bauer <konradbauer94@gmail.com>
2023-05-30 13:31:54 +0200

    add profile template look

--- src/layout/Main/index.tsx
+++ src/layout/Main/index.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Header, Input, LeftSide, RightSide, StyledMain } from "./styled";
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useDataLayerValue } from "../../core/DataLayer";
 
 export const Main = () => {
+  const [{ user }, dispatch] = useDataLayerValue();
+
   return (
-    <>
-      <StyledMain>
-        <Header>
-          <LeftSide>
-            <SearchSharpIcon />
-            <Input placeholder="Search for Artists, Songs, or Podcasts" type="text"></Input>
-          </LeftSide>
-          <RightSide />
-        </Header>
-      </StyledMain>
-    </>
+    <StyledMain>
+      <Header>
+        <LeftSide>
+          <SearchSharpIcon />
+          <Input placeholder="Search for Artists, Songs, or Podcasts" type="text" />
+        </LeftSide>
+        <RightSide>
+          <AccountCircleIcon src={user?.images[0]?.url} alt="User name" />
+          <span>Sakiro</span>
+        </RightSide>
+      </Header>
+    </StyledMain>
   );
 };
