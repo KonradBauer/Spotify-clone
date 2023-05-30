110eeabf66a9785ab65aef67473e39dfe96fc2a6
Konrad Bauer <konradbauer94@gmail.com>
2023-05-30 13:31:26 +0200

    styled header and input

--- src/layout/Main/styled.tsx
+++ src/layout/Main/styled.tsx
@@ -4,14 +4,18 @@ export const StyledMain = styled.div`
   display: grid;
   background: linear-gradient(rgb(91, 87, 115), rgba(0, 0, 0, 1));
   grid-area: 1 / 2 / 6 / 6;
-  padding: 20px;
+  padding: 30px;
 `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 30px;
+`;
 
 export const Input = styled.input`
   border: none;
-  outline: none;
+  width: 100%;
 `;
 
 export const LeftSide = styled.div`
@@ -20,10 +24,18 @@ export const LeftSide = styled.div`
   color: ${({ theme }) => theme.color.gray};
   border-radius: 30px;
   align-items: center;
-  justify-content: flex-start;
   max-width: 250px;
+  min-width: 80px;
+  flex-grow: 1;
   padding: 10px;
   height: 30px;
 `;
 
-export const RightSide = styled.div``;
+export const RightSide = styled.div`
+  display: flex;
+  justify-self: flex-end;
+  color: ${({ theme }) => theme.color.white};
+  gap: 5px;
+  margin-left: 10px;
+  cursor: pointer;
+`;
